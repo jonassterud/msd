0db36e54fc0ce8dceae253e13909148e5342277b
Jonas Sterud <57707537+jonassterud@users.noreply.github.com>
2021-03-21 21:27:31 +0100

    feat: web-ext config

---
+++ web-ext-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    verbose: true,
+    ignoreFiles: [
+        ".gitignore",
+        "README.md",
+        ".web-extension-id",
+        "web-ext-config.js"
+    ],
+    build: {
+        overwriteDest: true
+    }
+};
